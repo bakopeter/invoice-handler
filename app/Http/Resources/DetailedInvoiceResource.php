6c53cf348e5288c605a539fc466705676f98666a
bakopeter <bakopeter71@gmail.com>
2024-05-06 04:21:07 +0200

    Creating Api

---
+++ app/Http/Resources/DetailedInvoiceResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DetailedInvoiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => [
+                'id' => $this->id,
+                'invoiceNumber' => $this->invoiceNumber,
+                'invoiceIssueDate' => $this->invoiceIssueDate,
+                'supplierTP' => new DetailedTaxPayerResource($this->supplierTP),
+                'customerTP' => new DetailedTaxPayerResource($this->customerTP),
+                'invoiceDetail' => new DetailedInvoiceDetailResource($this->invoiceDetail),
+                'invoiceLines' => new InvoiceLineCollection($this->invoiceLines)
+            ],
+            'success' => true,
+            'message' => ""
+        ];
+    }
+}
